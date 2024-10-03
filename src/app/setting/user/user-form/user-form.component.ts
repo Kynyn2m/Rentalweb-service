c39e04f699e3854c0f6baca4137e342f1806bfec
Horng <kimhongrem@gmail.com>
2024-10-03 16:42:31 +0700

    add menu update user and delete

--- src/app/setting/user/user-form/user-form.component.ts
+++ src/app/setting/user/user-form/user-form.component.ts
@@ -1,9 +1,10 @@
 import { Component, HostListener, Inject } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, FormGroup, NgForm } from '@angular/forms';
 import { USER_FORM } from '../user-add/data.test';
 import { USER_TYPE } from '../user';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { UserService } from '../user.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-user-form',
@@ -15,55 +16,64 @@ export class UserFormComponent {
   user: USER_TYPE = new USER_TYPE();
   error = '';
   loading: boolean = false;
+  borderError: boolean = false;
+  departments: USER_TYPE[] = [];
+  isEditMode: boolean = false;
+  userForm: FormGroup;
 
   constructor(
     public dialogRef: MatDialogRef<UserFormComponent>,
     private userService: UserService,
-    @Inject(MAT_DIALOG_DATA) public data: USER_TYPE
+    private readonly snackBar: MatSnackBar,
+    @Inject(MAT_DIALOG_DATA) public data: USER_TYPE,
+    private _snackBar: MatSnackBar,
+    private fb: FormBuilder
   ) {
+    console.log('Received data:', data); // Check the data
     this.user = data;
+    this.userForm = this.fb.group({
+      fullName: [this.user.fullName],
+      email: [this.user.email],
+      gender: [this.user.gender],
+      username: [this.user.username],
+    });
   }
-
   ngOnInit(): void {}
 
   onSubmit(f: NgForm) {
-    // if (!f.valid) {
-    //   return;
-    // }
-    // if (this.user.id) {
-    //   this.userService.put(this.user.id, this.user).subscribe(
-    //     (data) => {
-    //       this.dialogRef.close();
-    //     }
-    //   );
-    // } else {
-    //   this.userService.post(this.user).subscribe(
-    //     (data) => {
-    //       this.dialogRef.close();
-    //     }
-    //   );
-    // }
-    // if (!f.valid) {
-    //   return
-    // }
-    // if (this.user.id) {
-    //   this.userService.put(this.user).subscribe(
-    //     (data) => {
-    //       this.dialogRef.close();
-    //     },
-    //     (error) => {
-    //       this.error = error.responseMessage;
-    //     }
-    //   );
-    // } else {
-    //   this.userService.post(this.user).subscribe(
+    if (!f.valid) {
+      return;
+    }
+    if (this.user.id) {
+      this.userService.updateUser(this.user, this.user.id).subscribe(
+        (data) => {
+          this.snackBar.open('User updated successfully!', 'Close', {
+            duration: 3000,
+          });
+          this.dialogRef.close();
+        },
+        (error) => {
+          console.error('Update error:', error); // Log the complete error for debugging
+          this.snackBar.open(
+            'Failed to update user. Please try again.',
+            'Close',
+            {
+              duration: 3000,
+            }
+          );
+          this.error = error.error?.message || 'An unknown error occurred.';
+        }
+      );
+    }
+    // else {
+    //   this.userService.createUser(this.user).subscribe(
     //     (data) => {
     //       this.dialogRef.close();
     //     },
     //     (error) => {
     //       this.error = error.responseMessage;
     //     }
-    //   )
+    //   );
     // }
   }
 
